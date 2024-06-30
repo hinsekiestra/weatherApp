79b118c0d40c53fc9c88ea6983bdd1f23ba3ee87
Hinse <110742648+hinsekiestra@users.noreply.github.com>
2024-06-30 14:30:15 +0200

    deleted unnecessary files and made new files clearer

---
+++ script.js
@@ -0,0 +1,51 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            api_key: 'ad7b75e05e8dd0be0f115e9dc85422bc',
+            url_base: 'https://api.openweathermap.org/data/2.5/',
+            query: '',
+            weather: {}
+        }
+    },
+    methods: {
+        fetchWeather(e) {
+            if (e.key == "Enter"){
+                fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+                .then(res => {
+                    return res.json();
+                }) .then(this.setResults);
+            }
+        },
+        setResults(results) {
+            this.weather = results;
+        },
+        dateBuilder() {
+            let d = new Date();
+            let months = ["January", "February", "March", "April", "May", "June", 
+            "July", "August", "September", "October", "November", "December"];
+            let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+            let day = days[d.getDay()];
+            let date = d.getDate();
+            let month = months[d.getMonth()];
+            let year = d.getFullYear();
+
+            return `${day} ${date} ${month} ${year}`;
+        },
+        windDirection(deg) {
+            let directions = ["North", "North/Northeast", "Northeast", "East/Northeast", "East", "East/Southeast", "Southeast", "South/Southeast", "South", "South/Southwest", "Southwest", "West/Southwest", "West", "West/Northwest", "Northwest", "North/Northwest"];
+            let index = (Math.round(deg / 22,5, 1));
+            let result = directions[index - 1];
+            return result;
+        },
+        arrowDirection(deg) {
+            let direction="transform: rotate(" + deg + "deg)";
+            console.log(deg);
+            console.log(direction);
+            return direction;
+        }
+    }
+})
+app.mount('#app');
+
+//https://api.openweathermap.org/data/2.5/weather?q=singapore&units=metric&APPID=ad7b75e05e8dd0be0f115e9dc85422bc
